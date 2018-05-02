e6443cd188831a7fd1033020fe0b942d4b0a0434
panyunyi <panyunyi@outlook.com>
2018-05-03 03:14:19 +0800

    增加发现页面UI
    N/A
    
    增加发现页面UI
    
    Signed-off-by: panyunyi <panyunyi@outlook.com>

---
+++ app.main/src/main/java/cn/panyunyi/healthylife/app/main/ui/activity/FoundActivity.java
@@ -0,0 +1,15 @@
+package cn.panyunyi.healthylife.app.main.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.panyunyi.healthylife.app.main.R;
+
+public class FoundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_found);
+    }
+}
